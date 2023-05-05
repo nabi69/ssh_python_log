e6ad82297f89579a37209cb91f581ede7d400f2d
M.Nabi <58561138+nabi69@users.noreply.github.com>
2023-05-05 14:38:07 +0200

    Update ssh_connect_command.py

--- ssh_connect_command.py
+++ ssh_connect_command.py
@@ -1,38 +1,18 @@
 import paramiko
-import datetime
+##Environment preparation and check ???? 
 
 ssh = paramiko.SSHClient()
 ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
-now = datetime.datetime.now()
-filename = now.strftime("%Y%m%d_%H%M%S") + '.txt'
-
-ip = input('Enter an IP address: ')
-port = input('Enter a port number: ')
-user = input('Enter username: ')
-password = input('Enter password: ')
-
 try:
-    # ssh.connect('158.174.251.106', port=8001, username='xpgroup', password='dhaka123')
-    ssh.connect(ip, port=port, username=user, password=password)
+    ssh.connect('158.174.251.106', port=8001, username='xpgroup', password='dhaka123')
     print('Connected to the server')
     while True:
-        command = input('Enter a Linux command: (exit to terminate) ')
+        command = input('Enter a Linux command: ')
         if command == 'exit':
             break
         stdin, stdout, stderr = ssh.exec_command(command)
-        # print('Command : '+ command)
-        # print('output:\n')
-        with open(filename, 'a') as f:
-            f.write('Command : '+ command)
-            f.write('\noutput:\n')
-            for line in stdout:
-                f.write(line.strip()+ '\n')
-                # print(line.strip())
-        # for line in stdout:
-        #     print(line.strip())
-
-
+        print(stdout.readlines())
 except paramiko.AuthenticationException:
     print('Authentication failed, please verify your credentials')
 except paramiko.SSHException as sshException:
