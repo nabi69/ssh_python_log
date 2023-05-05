086feeeea814cf8380df707c574468c06b2c6fc2
Ismail0817 <121042412+Ismail0817@users.noreply.github.com>
2023-05-05 14:37:40 +0200

    run command and save output in txt file

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# ssh_python_log
-analyze log
+This repo has two files. one is jupyter notebook and another on .py. if you just want to run the code then try .py.
+from your command prompt or just write python or python3 then the filename.py. if you want to play with the code try the jupyter
+notebook one.  
+
+
+
+What does the code do:
+1. it first takes inputs about the machine user want to connect. then connect to that machine via ssh. after successfull connection
+user can give command to the remote machine. if the command produces any output it will be saved in a txt file. txt files names are
+given according to time. so that it can be easier to find in the future
--- ssh_connect_command.py
+++ ssh_connect_command.py
@@ -1,17 +1,38 @@
 import paramiko
+import datetime
 
 ssh = paramiko.SSHClient()
 ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
+now = datetime.datetime.now()
+filename = now.strftime("%Y%m%d_%H%M%S") + '.txt'
+
+ip = input('Enter an IP address: ')
+port = input('Enter a port number: ')
+user = input('Enter username: ')
+password = input('Enter password: ')
+
 try:
-    ssh.connect('158.174.251.106', port=8001, username='xpgroup', password='dhaka123')
+    # ssh.connect('158.174.251.106', port=8001, username='xpgroup', password='dhaka123')
+    ssh.connect(ip, port=port, username=user, password=password)
     print('Connected to the server')
     while True:
-        command = input('Enter a Linux command: ')
+        command = input('Enter a Linux command: (exit to terminate) ')
         if command == 'exit':
             break
         stdin, stdout, stderr = ssh.exec_command(command)
-        print(stdout.readlines())
+        # print('Command : '+ command)
+        # print('output:\n')
+        with open(filename, 'a') as f:
+            f.write('Command : '+ command)
+            f.write('\noutput:\n')
+            for line in stdout:
+                f.write(line.strip()+ '\n')
+                # print(line.strip())
+        # for line in stdout:
+        #     print(line.strip())
+
+
 except paramiko.AuthenticationException:
     print('Authentication failed, please verify your credentials')
 except paramiko.SSHException as sshException:
